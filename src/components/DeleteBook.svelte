39dd2b86b765e3fb655e01a84d7424776af3dc73
Liseth Danae Mecatl <liseth.mecatl@alumno.buap.mx>
2024-05-19 23:24:11 -0600

    Componentes svelte book sin conexion con base de datos

---
+++ src/components/DeleteBook.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+    import { createEventDispatcher } from "svelte";
+    export let eliminar: number;
+
+    const dispatch = createEventDispatcher();
+
+    async function handleSubmit(event: Event) {
+        event.preventDefault();
+        const formData = new FormData(event.target as HTMLFormElement);
+        const id = formData.get("id");
+
+        try {
+            const response = await fetch(
+                `http://127.0.0.1:8000/API/a/book/${id}`,
+                {
+                    method: "DELETE",
+                    headers: {
+                        Authorization:
+                            "Bearer 9643c1a6845328c9a401658e988de1556a0fcd2b94eabd8ccd",
+                    },
+                },
+            );
+
+            if (response.ok) {
+                dispatch("bookDeleted", {
+                    message: "Libro eliminado con éxito.",
+                    type: "success",
+                });
+            } else {
+                const errorData = await response.json();
+                console.error("Error al eliminar el libro:", errorData);
+                dispatch("bookDeleted", {
+                    message: "No se pudo eliminar el libro.",
+                    type: "error",
+                });
+            }
+        } catch (error) {
+            console.error("Error al eliminar el libro:", error);
+            dispatch("bookDeleted", {
+                message: "Error de conexión.",
+                type: "error",
+            });
+        }
+    }
+</script>
+
+<form on:submit={handleSubmit}>
+    <h1>Eliminar registro de libro</h1>
+    <label for="id">ID del libro</label>
+    <input type="text" id="id" bind:value={eliminar} required />
+    <div id="btns">
+        <button type="submit" id="delete">Eliminar</button>
+    </div>
+</form>
+
+<style>
+    @media (width <= 1000px) {
+        form {
+            width: 80%;
+        }
+    }
+
+    @media (width > 1000px) {
+        form {
+            width: 30%;
+        }
+    }
+    form {
+        position: absolute;
+        top: 25%;
+        left: 30%;
+        width: 40%;
+        height: 40%;
+        padding: 4rem 1rem;
+        border-radius: 1rem;
+        display: flex;
+        background-color: #44365997;
+        justify-content: space-around;
+        flex-direction: column;
+        align-items: center;
+        box-shadow: 0.5rem 0.5rem 2rem #0004;
+        color: #fefefe;
+    }
+
+    h1 {
+        text-align: center;
+    }
+    #delete {
+        background-color: #ff2234;
+        width: 9rem;
+        color: #fefefe;
+        font-size: 0.9rem;
+        font-weight: bold;
+        padding: 0.2rem 1rem;
+        box-shadow: 0.2rem 0.2rem 1rem #464444;
+        margin: auto;
+        border-radius: 0.8rem;
+        text-align: center;
+        text-decoration: none;
+    }
+    #btns {
+        margin-top: 1rem;
+    }
+    #btns button:hover {
+        background-color: #ca0313;
+    }
+</style>
---
+++ src/components/Message.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    export let message: string;
+    export let type: "success" | "error" = "success";
+</script>
+
+<dialog>
+    <span class={`message ${type}`}>
+        {message}
+    </span>
+</dialog>
+
+<style>
+    dialog {
+        background-color: var(--primary);
+        border-radius: 0.5rem;
+        font-size: 2rem;
+        text-align: center;
+        color: #fefefe;
+    }
+
+    span {
+        flex-grow: 1;
+        font-family: "Lora", sans-serif;
+    }
+    /*svg {
+        width: -webkit-fill-available;
+        color: #fefefe;
+        font-size: 3.5rem;
+    }*/
+</style>
