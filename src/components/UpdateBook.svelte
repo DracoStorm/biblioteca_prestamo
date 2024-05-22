172e98e76c3e572734cd369e3b67a29ed562b527
DracoStorm <119288973+DracoStorm@users.noreply.github.com>
2024-05-22 01:15:19 -0600

    Fix styles in

--- src/components/UpdateBook.svelte
+++ src/components/UpdateBook.svelte
@@ -75,53 +75,51 @@
     <div id="id-section">
         <label for="id">ID del Libro</label>
         <input type="text" id="id" name="id" bind:value={id} required />
-        <div id="title-section">
-            <label for="title">Título</label>
-            <input
-                type="text"
-                id="title"
-                name="title"
-                bind:value={title}
-                required
-            />
-        </div>
-        <div id="author-section">
-            <label for="author">Autor</label>
-            <input
-                type="text"
-                id="author"
-                name="author"
-                bind:value={author}
-                required
-            />
-        </div>
-        <div id="editorial-section">
-            <label for="editorial">Editorial</label>
-            <select id="editorial" bind:value={editorial} required>
-                <option value="0" disabled selected>--Select an Option--</option
-                >
-                {#if editorials != undefined}
-                    {#each editorials as editorial}
-                        <option value={editorial.id}>{editorial.name}</option>
-                    {/each}
-                {/if}
-            </select>
-        </div>
-        <div id="category-section">
-            <label for="category">Categoria</label>
-            <select id="category" bind:value={category} required>
-                <option value="0" disabled selected>--Select an Option--</option
-                >
-                {#if categories != undefined}
-                    {#each categories as category}
-                        <option value={category.id}>{category.name}</option>
-                    {/each}
-                {/if}
-            </select>
-        </div>
-        <div id="btns">
-            <button type="submit" id="mod">Actualizar</button>
-        </div>
+    </div>
+    <div id="title-section">
+        <label for="title">Título</label>
+        <input
+            type="text"
+            id="title"
+            name="title"
+            bind:value={title}
+            required
+        />
+    </div>
+    <div id="author-section">
+        <label for="author">Autor</label>
+        <input
+            type="text"
+            id="author"
+            name="author"
+            bind:value={author}
+            required
+        />
+    </div>
+    <div id="editorial-section">
+        <label for="editorial">Editorial</label>
+        <select id="editorial" bind:value={editorial} required>
+            <option value="0" disabled selected>--Select an Option--</option>
+            {#if editorials != undefined}
+                {#each editorials as editorial}
+                    <option value={editorial.id}>{editorial.name}</option>
+                {/each}
+            {/if}
+        </select>
+    </div>
+    <div id="category-section">
+        <label for="category">Categoria</label>
+        <select id="category" bind:value={category} required>
+            <option value="0" disabled selected>--Select an Option--</option>
+            {#if categories != undefined}
+                {#each categories as category}
+                    <option value={category.id}>{category.name}</option>
+                {/each}
+            {/if}
+        </select>
+    </div>
+    <div id="btns">
+        <button type="submit" id="mod">Actualizar</button>
     </div>
 </form>
 
@@ -147,12 +145,11 @@
         padding: 2rem 1rem;
         border-radius: 1rem;
         display: grid;
-        grid-template-rows: repeat(4, 1fr);
-        grid-template-columns: repeat(3, 1fr);
+        grid-template-rows: repeat(5, 1fr);
+        grid-template-columns: repeat(2, 1fr);
         grid-template-areas:
-            "hs hs hs"
-            "title title autor" "editorial editorial category"
-            "place place ID";
+            "hs hs"
+            "id title" "autor category" "editorial editorial" "btn btn";
         gap: 1rem;
         font-family: "Bitter", sans-serif;
     }
@@ -163,32 +160,57 @@
         font-family: "Bitter", sans-serif;
     }
     #id-section {
-        margin: 0rem 5rem;
+        margin-right: 5rem;
+        margin-left: 2rem;
+        grid-area: id;
+    }
+    #title-section {
+        grid-area: title;
+        margin-right: 3rem;
+        margin-left: 2rem;
+    }
+    #author-section {
+        grid-area: autor;
+        margin-right: 3rem;
+        margin-left: 2rem;
+    }
+    #editorial-section {
+        grid-area: editorial;
+        margin-right: 2rem;
+        margin-left: 2rem;
+    }
+    #category-section {
+        grid-area: category;
+        margin-right: 2rem;
+        margin-left: 2rem;
     }
     input {
         width: 100%;
+    }
+    #hs {
+        grid-area: hs;
     }
     h1 {
         text-align: center;
     }
 
     button {
-        background-color: #8c618c;
+        background-color: var(--accent-1);
         width: 9rem;
-        color: #fefefe;
+        color: var(--accent-2);
+        border: none;
         font-size: 0.9rem;
         font-weight: bold;
         padding: 0.2rem 1rem;
-        box-shadow: 0.2rem 0.2rem 1rem #464444;
+        box-shadow: 0.2rem 0.2rem 1rem #0004;
         margin: auto;
         border-radius: 0.8rem;
         text-align: center;
         text-decoration: none;
     }
     #btns {
-        position: relative;
-        left: 11.5rem;
-        margin-top: 2rem;
+        display: flex;
+        grid-area: btn;
     }
     button:hover {
         background-color: #443659;
@@ -196,5 +218,6 @@
     select {
         width: 100%;
         border-radius: 1rem;
+        font-family: "Bitter";
     }
 </style>
